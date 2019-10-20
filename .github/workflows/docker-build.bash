9d26e651bb2e758e4a20e5fc3e688566f4b742fa
Sam Huynh <samhwang2112.dev@gmail.com>
2019-10-20 14:20:57 +1100

    Resolve error with docker image name

--- .github/workflows/docker-build.bash
+++ .github/workflows/docker-build.bash
@@ -1,10 +1,10 @@
 #!bin/bash
 
 # Build the base images
-docker build -f Dockerfile-apache-alpine -t ${DOCKER_USERNAME}/php:7.3 .
-docker build -f Dockerfile-apache-debian -t ${DOCKER_USERNAME}/php:7.3-nginx .
-docker build -f Dockerfile-nginx-alpine -t ${DOCKER_USERNAME}/php:7.3-apache-alpine .
-docker build -f Dockerfile-nginx-debian -t ${DOCKER_USERNAME}/php:7.3-nginx-alpine .
+docker build -f Dockerfile-apache-debian -t ${DOCKER_USERNAME}/php:7.3 .
+docker build -f Dockerfile-nginx-debian -t ${DOCKER_USERNAME}/php:7.3-nginx .
+docker build -f Dockerfile-apache-alpine -t ${DOCKER_USERNAME}/php:7.3-apache-alpine .
+docker build -f Dockerfile-nginx-alpine -t ${DOCKER_USERNAME}/php:7.3-nginx-alpine .
 
 # Tag apache-debian as the main image
 docker tag ${DOCKER_USERNAME}/php:7.3 ${DOCKER_USERNAME}/php:7.3-apache
