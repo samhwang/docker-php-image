13fd880667b19f72b45aae505c2e4be25ae2718c
Sam Huynh <samhwang2112.dev@gmail.com>
2019-10-20 13:07:20 +1100

    Add docker login to build process

--- .github/workflows/docker-build.bash
+++ .github/workflows/docker-build.bash
@@ -1,19 +1,20 @@
 #!bin/bash
 
 # Build the base images
-docker build -f Dockerfile-apache-alpine -t samhwang/php:7.3 .
-docker build -f Dockerfile-apache-debian -t samhwang/php:7.3-nginx .
-docker build -f Dockerfile-nginx-alpine -t samhwang/php:7.3-apache-alpine .
-docker build -f Dockerfile-nginx-debian -t samhwang/php:7.3-nginx-alpine .
+docker build -f Dockerfile-apache-alpine -t ${DOCKER_USERNAME}/php:7.3 .
+docker build -f Dockerfile-apache-debian -t ${DOCKER_USERNAME}/php:7.3-nginx .
+docker build -f Dockerfile-nginx-alpine -t ${DOCKER_USERNAME}/php:7.3-apache-alpine .
+docker build -f Dockerfile-nginx-debian -t ${DOCKER_USERNAME}/php:7.3-nginx-alpine .
 
 # Tag apache-debian as the main image
-docker tag samhwang/php:7.3 samhwang/php:7.3-apache
-docker tag samhwang/php:7.3 samhwang/php:latest
+docker tag ${DOCKER_USERNAME}/php:7.3 ${DOCKER_USERNAME}/php:7.3-apache
+docker tag ${DOCKER_USERNAME}/php:7.3 ${DOCKER_USERNAME}/php:latest
 
 # Push to Docker Cloud
-docker push samhwang/php:latest
-docker push samhwang/php:7.3
-docker push samhwang/php:7.3-apache
-docker push samhwang/php:7.3-nginx
-docker push samhwang/php:7.3-apache-alpine
-docker push samhwang/php:7.3-nginx-alpine
+docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD} docker.io
+docker push ${DOCKER_USERNAME}/php:latest
+docker push ${DOCKER_USERNAME}/php:7.3
+docker push ${DOCKER_USERNAME}/php:7.3-apache
+docker push ${DOCKER_USERNAME}/php:7.3-nginx
+docker push ${DOCKER_USERNAME}/php:7.3-apache-alpine
+docker push ${DOCKER_USERNAME}/php:7.3-nginx-alpine
