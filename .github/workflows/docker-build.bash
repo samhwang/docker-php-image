684df2f01af0e27d802105982125ffc715960a7c
Sam Huynh <samhwang2112.dev@gmail.com>
2019-10-23 09:54:27 +1100

    Logout after finish pushing

--- .github/workflows/docker-build.bash
+++ .github/workflows/docker-build.bash
@@ -1,5 +1,7 @@
 #!bin/bash
 
+docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD} docker.io
+
 # Build the base images
 docker build -f Dockerfile-apache-debian -t ${DOCKER_USERNAME}/php:7.3 .
 docker build -f Dockerfile-nginx-debian -t ${DOCKER_USERNAME}/php:7.3-nginx .
@@ -11,10 +13,11 @@ docker tag ${DOCKER_USERNAME}/php:7.3 ${DOCKER_USERNAME}/php:7.3-apache
 docker tag ${DOCKER_USERNAME}/php:7.3 ${DOCKER_USERNAME}/php:latest
 
 # Push to Docker Cloud
-docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD} docker.io
 docker push ${DOCKER_USERNAME}/php:latest
 docker push ${DOCKER_USERNAME}/php:7.3
 docker push ${DOCKER_USERNAME}/php:7.3-apache
 docker push ${DOCKER_USERNAME}/php:7.3-nginx
 docker push ${DOCKER_USERNAME}/php:7.3-apache-alpine
 docker push ${DOCKER_USERNAME}/php:7.3-nginx-alpine
+
+docker logout docker.io
